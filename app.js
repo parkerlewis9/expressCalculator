cce12d4a811fd123f5ad72b4c48019236011900c
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-06-03 17:01:00 -0700

    fimish bonuses

--- app.js
+++ app.js
@@ -9,18 +9,34 @@ app.get("/add/:num1/:num2", function(req, res) {
 })
 
 app.get("/subtract/:num1/:num2", function(req, res) {
-	var sum = parseFloat(req.params.num1) - parseFloat(req.params.num2);
-	res.render("math", {number: sum})
+	var difference = parseFloat(req.params.num1) - parseFloat(req.params.num2);
+	res.render("math", {number: difference})
 })
 
 app.get("/multiply/:num1/:num2", function(req, res) {
-	var sum = parseFloat(req.params.num1) * parseFloat(req.params.num2);
-	res.render("math", {number: sum})
+	var product = parseFloat(req.params.num1) * parseFloat(req.params.num2);
+	res.render("math", {number: product})
 })
 
 app.get("/divide/:num1/:num2", function(req, res) {
-	var sum = parseFloat(req.params.num1) / parseFloat(req.params.num2);
-	res.render("math", {number: sum})
+	var quotient = parseFloat(req.params.num1) / parseFloat(req.params.num2);
+	res.render("math", {number: quotient})
+})
+
+app.get("/math/:operator/:num1/:num2", function(req, res) {
+	if(req.params.operator === "add") {
+		var sum = parseFloat(req.params.num1) + parseFloat(req.params.num2);
+		res.render("math", {number: sum})
+	} else if (req.params.operator === "subract") {
+		var difference = parseFloat(req.params.num1) - parseFloat(req.params.num2);
+		res.render("math", {number: difference});
+	} else if (req.params.operator === "multiply") {
+		var product = parseFloat(req.params.num1) * parseFloat(req.params.num2);
+		res.render("math", {number: product})
+	} else if (req.params.operator === "divide") {
+		var quotient = parseFloat(req.params.num1) / parseFloat(req.params.num2);
+		res.render("math", {number: quotient})
+	}
 })
 
 
